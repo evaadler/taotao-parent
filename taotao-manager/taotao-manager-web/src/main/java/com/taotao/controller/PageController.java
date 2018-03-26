06e8dd8e8a5568320a7a84a4aab146be5f6770dc
alicia <jiandanfifi>
2018-03-26 20:54:10 +0800

    整体框架以及基本sql方法测试调通

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,27 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    /**
+     * 打开首页
+     */
+    @RequestMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+
+    /**
+     * 展示其他页面
+     * @param page  @PathVariable 从url中提取参数
+     * @return
+     */
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page){
+        return page;
+    }
+}
